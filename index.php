8625d484502f71f11e491c51de2c3d78b93ef50a
maneldograu244 <emanuelcristo2016@gmail.com>
2025-02-10 20:21:23 -0300

    aula10/02

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portifólio</title>
+
+    <link rel="stylesheet" href= "css/style.css" />
+</head>
+<body>
+
+<div class="interface">
+ 
+     <header class="cabecalho">
+
+       <nav class="menu">
+
+        <div class="logo">
+
+          <img src="imagens/logo.png" alt="LOGO" width="150"/>
+
+        </div>
+           
+               <ul>
+                  
+                 <li>Home</li>
+                 <li>Serviços</li>
+                 <li>Produtos</li>
+                 <li>Sobre</li>
+                 <li>Contato</li>
+                 <li>Portifolio</li>
+
+               </ul>
+
+       </nav>
+
+       <div class="titles">
+
+         <h1 class="Title"> Entregando Serviços de Qualidade </h1>
+         <h1 class="Subtitle"> Soluções em TI</h1>
+
+            <p class="paragrafo">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum dolores nam illum sint, quisquam vero! </p>
+
+       </div>
+
+          <div class="class socialIcons"></div>
+          <a href="#">Twitter</a>
+          <a href="#">Facebook</a>
+          <a href="#">LinkedIn</a>
+          <a href="#">Instagram</a>
+
+    </header>
+
+</div>
+    
+</body>
+</html>
