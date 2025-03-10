aa4fce2bb5221f095c23c02724d9f3191dce842c
maneldograu244 <emanuelcristo2016@gmail.com>
2025-03-10 20:37:46 -0300

    10/03

--- index.php
+++ index.php
@@ -5,16 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Portifólio</title>
 
+    <link rel="stylesheet" href="tools/fontawesome/css/all.css" />
     <link rel="stylesheet" href="css/estilos.css" />
 
-   
 </head>
 <body>
 
 <div id="interface">
 
     <header class="cabecalho">
-
 
         <nav class="menu">
 
@@ -23,36 +22,35 @@
             </div>
 
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Serviços</a></li>
-                <li><a href="#">Produtos</a></li>
-                <li><a href="#">Portifólio</a></li>
+                <li><a href="#" title= "Visite o nosso Twitter!">Home</a></li>
+                <li><a href="#" title= "Visite o nosso Facebook!" >Serviços</a></li>
+                <li><a href="#" title= "Visite o nosso Lindekin!">Produtos</a></li>
+                <li><a href="#" title= "Visite o nosso Instagram!" >Portifólio</a></li>
                 <li><a href="#">Sobre</a></li>
                 <li><a href="#">Contato</a></li>
             </ul>
 
         </nav>
-
-        <div class="titles">
+        
+        <div class="titles"> <!-- ========================= -->
             <div class="container-titles">
+                <h1 class="titulo">Entregando Serviços de Qualidade</h1>
+                <h1 class="subtitulo">Soluções em TI</h1>
 
-               <h1 class="titulo">Entregando serviços de qualidade</h1>
-               <h1 class="subtitulo">Soluções em TI</h1>
-
-               <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ab in! A, nam voluptas!</p>
+                <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vitae rerum, necessitatibus quod quaerat mollitia.</p>
+                
             </div>
 
-           <div class="socialIcons">
-             <a href="#">Twitter</a>
-             <a href="#">Facebook</a>
-             <a href="#">LinkedIn</a>
-             <a href="#">Instagram</a>
-           </div> 
-         </div>
-
+            <div class="socialIcons">
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-linkedin"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div> <!-- ========================== -->
+        
     </header>
 
-    
 </div>
     
 </body>
